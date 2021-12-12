da3bf4c9b52e47e06fa014d4338c25b581a2a86b
MickeyMiguel97 <00145016@uca.edu.sv>
2021-12-12 00:58:51 -0600

    Detalle del tipo de voluntario

--- src/views/TipoVoluntario/_id/index.vue
+++ src/views/TipoVoluntario/_id/index.vue
@@ -1,13 +1,13 @@
 <template>
   <main class="pb-40">
-    <page-heading title="Detalle sede" back-route="/sede" :breadcrumbs="breadcrumbs">
+    <page-heading title="Detalle del Tipo de Voluntario" back-route="/tipoVoluntario" :breadcrumbs="breadcrumbs">
       <template slot="actions">
         <custom-button
           title="Editar"
           color="white"
           size="small"
           class="mr-2"
-          :to="`/sede/${sede.id}/edit`"
+          :to="`/tipoVoluntario/${tipoVoluntario.id}/edit`"
         >
           <template #icon>
             <svg
@@ -25,67 +25,67 @@
             </svg>
           </template>
         </custom-button>
+        <custom-button
+          title="Eliminar"
+          color="secondary"
+          size="small"
+          @click="showDeleteModal = true"
+        >
+          <template #icon>
+            <svg
+              class="w-4 h-4 mr-2"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+          </template>
+        </custom-button>
       </template>
     </page-heading>
-    <section v-if="sede">
+    <section v-if="tipoVoluntario">
       <div class="overflow-hidden bg-white border border-gray-100 shadow sm:rounded-lg">
         <div class="px-4 py-5 border-b border-gray-100 sm:px-6">
           <h3 class="text-lg font-medium leading-6 text-gray-900">
-            Sede #{{ sede.id }}
+            Tipo de Voluntario #{{ tipoVoluntario.id }}
           </h3>
           <p class="max-w-2xl mt-1 text-sm leading-5 text-gray-500">
-            Información general de la sede {{ sede.code }}.
+            Información general del Tipo de Voluntario {{ tipoVoluntario.tipo }}.
           </p>
         </div>
         <div>
           <dl>
             <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt class="text-sm font-medium leading-5 text-gray-500">
-                Nombre
+                Tipo
               </dt>
               <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                {{ sede.name }}
-              </dd>
-            </div>
-            <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium leading-5 text-gray-500">
-                Código
-              </dt>
-              <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                {{ sede.code }}
-              </dd>
-            </div>
-            <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium leading-5 text-gray-500">
-                Logo
-              </dt>
-              <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                <img v-if="sede.logo" :src="sede.logo" alt="">
-              </dd>
-            </div>
-            <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium leading-5 text-gray-500">
-                Dirección
-              </dt>
-              <dd
-                class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
-              >
-                {{ sede.address }}
-              </dd>
-            </div>
-            <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium leading-5 text-gray-500">
-                Activo
-              </dt>
-              <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                <active-indicator :status="Boolean(sede.active)" />
+                {{ tipoVoluntario.tipo }}
               </dd>
             </div>
           </dl>
         </div>
       </div>
     </section>
-    <loading :active="isSedeLoading" :is-full-page="false" />
+    <delete-item
+      :show="showDeleteModal"
+      :title="`Tipo de voluntario ${tipoVoluntario.tipo}`"
+      @update:show="showDeleteModal = false"
+      @action="deleteItem"
+    />
+    <confirmation-modal
+      :show="showConfirmationModal"
+      title="Tipo de Voluntario Eliminado"
+      @update:show="showConfirmationModal = false"
+      @action="redirectBack"
+    />
+    <loading :active="isTipoVoluntarioLoading" :is-full-page="false" />
   </main>
 </template>
 
@@ -97,13 +97,15 @@ import PageHeading from '@/components/layout/PageHeading.vue';
 import CustomButton from '@/components/ui/CustomButton.vue';
 import ActiveIndicator from '@/components/ui/ActiveIndicator.vue';
 import Loading from '@/components/ui/Loading.vue';
+import DeleteItem from '@/components/general/DeleteItem.vue';
+import ConfirmationModal from '@/components/general/ConfirmationModal.vue';
 
 interface Breadcrumb {
   name: string;
   route?: string;
 }
 
-const Sedes = namespace('sede');
+const TiposVoluntarios = namespace('tipoVoluntario');
 
 @Component({
   components: {
@@ -111,29 +113,40 @@ const Sedes = namespace('sede');
     CustomButton,
     ActiveIndicator,
     Loading,
+    DeleteItem,
+    ConfirmationModal,
   },
 })
-export default class ShowSedePage extends Vue {
+export default class ShowTipoVoluntarioPage extends Vue {
+  showConfirmationModal = false
+  showDeleteModal = false
   breadcrumbs: Breadcrumb[] = [
     { name: 'Administración' },
-    { name: 'Sede', route: '/sede' },
+    { name: 'TipoVoluntario', route: '/tipoVoluntario' },
     { name: 'Detalle' },
   ]
 
-  @Sedes.State('isLoading') isSedeLoading!: boolean;
-  @Sedes.State('sede') sede!: Sede;
-  @Sedes.Action('show') fetchSede!: ({ id, vm }: { id: number; vm: any }) => ActionMethod
+  @TiposVoluntarios.State('isLoading') isTipoVoluntarioLoading!: boolean;
+  @TiposVoluntarios.State('tipoVoluntario') tipoVoluntario!: TipoVoluntario;
+  @TiposVoluntarios.Action('show') fetchTipoVoluntario!: ({ id, vm }: { id: number; vm: any }) => ActionMethod
+  @TiposVoluntarios.Action('destroy') deleteTipoVoluntario!: ({ id, vm }: { id: number; vm: any }) => ActionMethod
 
   async mounted() {
     const { id } = this.$route.params;
-    await this.fetchSede({ id: +id, vm: this });
-    if (!this.sede.id) {
+    await this.fetchTipoVoluntario({ id: +id, vm: this });
+    if (!this.tipoVoluntario.id) {
       this.redirectBack();
     }
   }
 
+  async deleteItem() {
+    const id = Number(this.tipoVoluntario?.id);
+    await this.deleteTipoVoluntario({ id, vm: this });
+    this.showConfirmationModal = true;
+  }
+
   redirectBack() {
-    this.$router.push('/sede');
+    this.$router.push('/tipoVoluntario');
   }
 }
 </script>
