acabaea4700c1e30c01e7307424d995f285e2fbf
Marvin Ramirez <00120717@uca.edu.sv>
2022-04-02 21:56:36 -0600

    fixing map

--- src/views/emergenciaRealizada/new.vue
+++ src/views/emergenciaRealizada/new.vue
@@ -1,9 +1,9 @@
 <template>
    <main class="pb-40">
       <page-heading title="Nueva Emergencia Realizada" back-route="/emergenciaRealizada" :breadcrumbs="breadcrumbs" />
+      <LocationSelectorMap v-model="location">
+      </LocationSelectorMap>
       <form-section>
-        <LocationSelectorMap v-model="location" >
-        </LocationSelectorMap>
          <ValidationObserver ref="form" tag="form" autocomplete="off" @submit.prevent="onSubmit">
             <div class="md:grid md:grid-cols-3 md:gap-8 ">
                <div class="md:col-span-1">
